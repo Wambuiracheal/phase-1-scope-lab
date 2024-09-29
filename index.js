faddaacb846c363d59e79efef4480726612fa553
Racheal <rachealnjugush@gmail.com>
2024-09-29 15:48:09 +0300

    myfunctions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+//test1
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+//test2
+var bestCustomer;
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+//test3
+function overwriteBestCustomer(){
+   bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Jane'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Mark'
+}

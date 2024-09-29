4ede2f402972c1829996fdae38cea027408d328f
Racheal <rachealnjugush@gmail.com>
2024-09-29 15:52:04 +0300

    scope

--- index.js
+++ index.js
@@ -2,21 +2,27 @@
 //test1
 var customerName = 'bob'
 
+//function1
 function upperCaseCustomerName(){
     customerName = customerName.toUpperCase();
 }
 
 //test2
+//function2
 var bestCustomer;
+
 function setBestCustomer(){
-    bestCustomer = 'not bob';
+    bestCustomer = 'not bob'
 }
 
 //test3
+//function3
 function overwriteBestCustomer(){
    bestCustomer = 'maybe bob'
 }
 
+//test4
+//function4
 const leastFavoriteCustomer = 'Jane'
 
 function changeLeastFavoriteCustomer(){
